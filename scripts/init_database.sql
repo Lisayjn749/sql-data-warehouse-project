8ae6966e092afefb3d4cfb622e88dcf98a276562
Lisayjn749 <lisayuyjn@gmail.com>
2025-03-26 19:25:42 -0400

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,26 @@
+USE master;
+go
 
+-- Drop and recreate the 'DataWareHouse' database
+if exists (select 1 from sys.databases where name = 'DataWareHouse')
+  begin 
+    alter database DataWareHouse set SINGLE_USER with rollback immediate;
+    drop database DataWareHouse;
+  end;
+go
+  
+-- Create the 'DataWareHouse' database 
+create DATABASE DataWareHouse;
+go
+  
+use DataWareHouse;
+go
+
+create schema bronze;
+go
+  
+create schema silver;
+go
+  
+create schema gold;
+go
